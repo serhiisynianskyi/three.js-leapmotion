2f82deb68bf3a6457380341c2280f22d1b482d13
Sergius <kfzadrser@gmail.com>
2018-04-22 23:34:33 +0300

    update(script): change styles of mesh

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 window.onload = function() {
 	let canvas = document.getElementById('canvas');
 	let scene = new THREE.Scene();
-	let camera, renderer, light, controls, stats;
+	let camera, renderer, light, controls, stats, textureCube;
 
 	function initScene() {
 		camera = new THREE.PerspectiveCamera(65, window.innerWidth / window.innerHeight, 0.1, 10000);
@@ -18,6 +18,12 @@ window.onload = function() {
 		renderer.toneMapping = THREE.LinearToneMapping;
 		// renderer.toneMappingExposure = 1;
 		renderer.setClearColor(0xffffff);
+		textureCube = new THREE.CubeTextureLoader()
+			.setPath('./cube/dark_dust/')
+			.load(['sleepyhollow_ft.jpg', 'sleepyhollow_bk.jpg', 'sleepyhollow_up.jpg', 'sleepyhollow_dn.jpg', 'sleepyhollow_rt.jpg', 'sleepyhollow_lf.jpg']);
+		// 	// .setPath('./cube/space/')
+			// .load(['2.png', '4.png', '5.png', '6.png', '3.png', '1.png']);
+		// scene.background = textureCube;
 	}
 
 	function resize() {
@@ -40,11 +46,27 @@ window.onload = function() {
 		light.shadow.camera.top = d;
 		light.shadow.camera.bottom = -d;
 		light.shadow.camera.far = 2000;
-		scene.add(new THREE.AmbientLight(0xffffff, 0.2));
+		scene.add(new THREE.AmbientLight(0xffffff, 1));
 		scene.add(light);
 	}
+
+	function createSceneBackground(currentMap) {
+		let cubeGeometry = new THREE.CubeGeometry(6000, 6000, 6000),
+			cubeMesh = new THREE.Mesh(cubeGeometry, new THREE.MeshFaceMaterial([
+				new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('cube/dark_dust/sleepyhollow_ft.jpg'), side: THREE.DoubleSide }),
+				new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('cube/dark_dust/sleepyhollow_bk.jpg'), side: THREE.DoubleSide }),
+				new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('cube/dark_dust/sleepyhollow_up.jpg'), side: THREE.DoubleSide }),
+				new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('cube/dark_dust/sleepyhollow_dn.jpg'), side: THREE.DoubleSide }),
+				new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('cube/dark_dust/sleepyhollow_rt.jpg'), side: THREE.DoubleSide }),
+				new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('cube/dark_dust/sleepyhollow_lf.jpg'), side: THREE.DoubleSide })
+			]));
+		// cubeMesh.rotation.set(0, 0.95, 0);
+		scene.add(cubeMesh);
+	}
+
 	initScene();
 	addLights();
+	createSceneBackground();
 	rendering();
 
 	controls = new THREE.OrbitControls(camera);
@@ -90,13 +112,14 @@ window.onload = function() {
 					});
 				});
 				let armMesh = hand.data('armMesh');
+				// debugger
 				armMesh.position.fromArray(hand.arm.center());
 				armMesh.setRotationFromMatrix(
 					(new THREE.Matrix4).fromArray(hand.arm.matrix())
 				);
 				armMesh.quaternion.multiply(baseBoneRotation);
-				armMesh.scale.x = hand.arm.width / 3;
-				armMesh.scale.z = hand.arm.width / 4;
+				armMesh.scale.x = hand.arm.width / 2;
+				armMesh.scale.z = hand.arm.width / 2;
 			}
 		})
 		// these two LeapJS plugins, handHold and handEntry are available from leapjs-plugins, included above.
@@ -117,47 +140,50 @@ window.onload = function() {
 					
 
 					if (index === 3 ){
-						boneType = new THREE.CylinderGeometry(8,5,bone.length,32,2)
+						boneType = new THREE.CylinderGeometry(10,6.9,bone.length,32,2)
 					}
 					else if (index === 0 ){
 						if (fingerIndex !==0){
-							boneType = new THREE.CylinderGeometry(20,10,bone.length,32,2)
+							boneType = new THREE.CylinderGeometry(25,13,bone.length,32,2)
 						}
 					}
 					else if (index === 1){
-						boneType = new THREE.CylinderGeometry(10,8,bone.length,32,2)	
+						boneType = new THREE.CylinderGeometry(13,11,bone.length,32,2)	
 					}	
 					else {
-						boneType = new THREE.CylinderGeometry(8, 8, bone.length,80)
+						boneType = new THREE.CylinderGeometry(10, 10, bone.length,80)
 					}
 					if (fingerIndex === 0 ){
 						if (index === 1) {
-							boneType = new THREE.CylinderGeometry(15,10,bone.length,32,2)
+							boneType = new THREE.CylinderGeometry(20,13,bone.length,32,2)
 						}
 						if (index === 2) {
-							boneType = new THREE.CylinderGeometry(9,8,bone.length,32,2)
+							boneType = new THREE.CylinderGeometry(13,10,bone.length,32,2)
 						}
 						
 					}
 					let boneMesh = new THREE.Mesh(
 						boneType,
-						new THREE.MeshStandardMaterial({metalness: 0.6, roughness: 0.5, color:0x999999, map: giftBump})
+						new THREE.MeshStandardMaterial({metalness: 0.9, roughness: 0.4, color:0x450000, envMap: textureCube})
 					);
 					scene.add(boneMesh);
 					boneMeshes.push(boneMesh);
 				});
 				for (let i = 0; i < finger.bones.length + 1; i++) {
-					let jointMeshGeometryParam = 7,
-						material = new THREE.MeshStandardMaterial({metalness: 0.9, roughness: 0.6, color:0x111111});
+					let jointMeshGeometryParam = 9.5,
+						material = new THREE.MeshStandardMaterial({metalness: 0.4, roughness: 0.6, color:0x010101});
 					if (i === 4) {
-						jointMeshGeometryParam = 4.95;
-						material = new THREE.MeshStandardMaterial({metalness: 0.6, roughness: 0.5, color:0x999999, map: giftBump});
+						jointMeshGeometryParam = 6.8;
+						material = new THREE.MeshStandardMaterial({metalness: 0.9, roughness: 0.4, color:0x450000, envMap: textureCube});
+					}
+					if (i === 3) {
+						jointMeshGeometryParam = 9;
 					}
 					else if (i === 0) {
-						jointMeshGeometryParam = 17;
+						jointMeshGeometryParam = 24;
 					}
 					else if(i === 1) {
-						jointMeshGeometryParam = 9;	
+						jointMeshGeometryParam = 12.2;	
 					}
 					let jointMesh = new THREE.Mesh(
 						new THREE.SphereGeometry(jointMeshGeometryParam,20,20),
@@ -169,12 +195,13 @@ window.onload = function() {
 				finger.data('boneMeshes', boneMeshes);
 				finger.data('jointMeshes', jointMeshes);
 			});
+			console.log(hand)
 			// console.log(hand);
 			if (hand.arm) { // 2.0.3+ have arm api,
 				// CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded)
 				let armMesh = new THREE.Mesh(
 					new THREE.CylinderGeometry(2.5,.9,hand.arm.length + 10,32,2),
-					new THREE.MeshStandardMaterial({metalness: 0.6, roughness: 0.5, color:0x999999, map: giftBump})
+					new THREE.MeshStandardMaterial({metalness: 0.8, roughness: 0.5, color:0x8c0000, envMap: textureCube})
 				);
 				armMesh.material.color.setHex(0xffffff);
 				scene.add(armMesh);
